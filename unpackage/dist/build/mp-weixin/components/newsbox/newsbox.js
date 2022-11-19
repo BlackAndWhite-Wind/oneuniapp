5ea0d4fbb3588c8dfa01349f0f428b2e69b0a3ec
BlackAndWhite-Wind <2020141460308@stu.scu.edu.cn>
2022-11-19 15:36:43 +0800

    commit

---
+++ unpackage/dist/build/mp-weixin/components/newsbox/newsbox.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["components/newsbox/newsbox"],{1494:function(t,n,e){"use strict";var u=e("6c60"),r=e.n(u);r.a},"2df3":function(t,n,e){"use strict";e.r(n);var u=e("6656"),r=e.n(u);for(var a in u)"default"!==a&&function(t){e.d(n,t,(function(){return u[t]}))}(a);n["default"]=r.a},6656:function(t,n,e){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default=void 0;var u={name:"newsbox",props:{item:{type:Object,default:function(){return{title:"默认标题",author:"张三",hits:123,picurl:"../../static/images/nopic.jpg"}}}},data:function(){return{}}};n.default=u},"6c60":function(t,n,e){},"8a10":function(t,n,e){"use strict";var u;e.d(n,"b",(function(){return r})),e.d(n,"c",(function(){return a})),e.d(n,"a",(function(){return u}));var r=function(){var t=this,n=t.$createElement;t._self._c},a=[]},a5d0:function(t,n,e){"use strict";e.r(n);var u=e("8a10"),r=e("2df3");for(var a in r)"default"!==a&&function(t){e.d(n,t,(function(){return r[t]}))}(a);e("1494");var c,o=e("f0c5"),i=Object(o["a"])(r["default"],u["b"],u["c"],!1,null,null,null,!1,u["a"],c);n["default"]=i.exports}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/newsbox/newsbox-create-component',
+    {
+        'components/newsbox/newsbox-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('543d')['createComponent'](__webpack_require__("a5d0"))
+        })
+    },
+    [['components/newsbox/newsbox-create-component']]
+]);
